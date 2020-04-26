07f9e4bebe2393183a715d9e1fd393ca8ee35538
BekeWorker <1975269079@qq.com>
2020-04-26 13:00:51 +0800

    '完成TabBarControl的吸顶效果'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="home" >
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <tab-bar-control class="tab-control1"
+                     :controlText="controlText"
+                     @clickControl="clickControl"
+                     ref="tabControl1"
+                     v-show="isShowControl"
+    />
     <scroll class="content"
             ref="scroll"
             :probeType="3"
@@ -8,12 +14,13 @@
             :pullUpLoad="true"
             @pullUpLoad="loadMore"
     >
-        <home-swiper :banners="banners"/>
+        <home-swiper :banners="banners" @swiperImgLoad="controlOffsetTop"/>
         <home-recommend :recommends="recommends"/>
         <feature-view/>
-        <tab-bar-control class="tab-control"
-                         :controlText="controlText"
-                         @clickControl="clickControl"/>
+        <tab-bar-control :controlText="controlText"
+                         @clickControl="clickControl"
+                         ref="tabControl2"
+        />
         <goods-list :goods="showGoods" />
     </scroll>
     <back-top @click.native="backClick" v-show="isShow"/>
@@ -62,7 +69,9 @@
         },
         goodsType: 'pop',
         isShow: false,
-        setTop: 0
+        setTop: 0,
+        offsetTopControl: 0,
+        isShowControl: false
       }
     },
     created() {
@@ -127,22 +136,30 @@
             break;
           }
         }
+        this.$refs.tabControl1.finalIndex = index
+        this.$refs.tabControl2.finalIndex = index
       },
       // 监听组件的点击,点击后滚动到顶部
       backClick() {
-        // console.log('backTop发生了点击');
         // 组件访问$refs,访问子组件
         this.$refs.scroll.scrollTo(0, 0, 500)
       },
       // 获得当前滚动的位置
       getPosition(position) {
         // 判断backTop的隐藏和显示
-        this.isShow = -position.y >1000
+        this.isShow = -position.y > 1000
+        this.isShowControl = -position.y > this.offsetTopControl
       },
       // 上拉加载更多
       loadMore () {
         this.getHomeGoodsData(this.goodsType)
       },
+      // 获取offsetTopControl正确的高度
+      controlOffsetTop () {
+        // 获取backTop的高度
+        this.offsetTopControl = this.$refs.tabControl2.$el.offsetTop
+        console.log(this.offsetTopControl);
+      }
     }
   }
 </script>
@@ -175,6 +192,15 @@
     overflow: hidden;
     height: calc(100% - 93px);
     margin-top: 44px;
+
+  }
+  #home .tab-control1 {
+    position: fixed;
+    top: 44px;
+    left: 0;
+    right: 0;
+    z-index: 9;
+    /*background-color: #fff;*/
   }
 
 </style>
--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -3,7 +3,7 @@
     <swiper>
       <swiper-item v-for="item in banners">
         <a :href="item.link">
-          <img :src="item.image" alt="">
+          <img :src="item.image" alt="" @load="swiperImgLoad">
         </a>
       </swiper-item>
     </swiper>
@@ -19,11 +19,24 @@
       SwiperItem
     },
     props: {
-      banners:{
-        type:Array,
+      banners: {
+        type: Array,
         default ()  {
           return []
         }
+      }
+    },
+    data() {
+      return {
+        isEmit: true
+      }
+    },
+    methods: {
+      swiperImgLoad() {
+        if(this.isEmit) {
+          this.$emit('swiperImgLoad')
+        }
+        this.isEmit = false
       }
     }
   }
